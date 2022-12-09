ff7e0e8bee7833697737210ef655cd36b95c3fcb
Abdihakim Hassan <h.abdihakim01@gmail.com>
2022-12-09 08:58:55 +0000

    Edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  *main- print a line of code using puts
  * 
- * Return: 0
+ * Return: 0(Success);
  */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
+   puts("\"Programming is like building a multilingual puzzle");
    return 0; 
 }
