88caa0a281c18242bb1f72e2709de27da89c2b10
Abdihakim Hassan <h.abdihakim01@gmail.com>
2022-12-13 20:15:19 +0000

    Numberzzzzz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar((num % 10) + '0');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
